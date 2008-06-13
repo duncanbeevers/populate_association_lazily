c0410db2b0e22bdce4dc2188085bcf40d8e33d8f
Duncan Beevers <duncanbeevers@gmail.com>
2008-06-13 11:03:51 -0700

    Mix behavior into HasManyAssociation

--- init.rb
+++ init.rb
@@ -2,7 +2,11 @@ require 'populate_association_lazily'
 
 [ ActiveRecord::Associations::BelongsToAssociation,
   ActiveRecord::Associations::HasOneAssociation
-
 ].each do |klass|
   klass.send(:include, PopulateAssociationLazily::PopulateSingleAssocationLazily)
+end
+
+[ ActiveRecord::Associations::HasManyAssociation
+].each do |klass|
+  klass.send(:include, PopulateAssociationLazily::PopulateMultipleAssociationLazily)
 end
