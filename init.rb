659c28c3a3cef918318ce65e74f5f083572648e6
Duncan Beevers <duncanbeevers@gmail.com>
2008-06-13 12:37:28 -0700

    Simplify mix into AssociationCollection

--- init.rb
+++ init.rb
@@ -6,7 +6,5 @@ require 'populate_association_lazily'
   klass.send(:include, PopulateAssociationLazily::PopulateSingleAssocationLazily)
 end
 
-[ ActiveRecord::Associations::AssociationCollection
-].each do |klass|
-  klass.send(:include, PopulateAssociationLazily::PopulateMultipleAssociationLazily)
-end
+ActiveRecord::Associations::AssociationCollection.send :include,
+  PopulateAssociationLazily::PopulateMultipleAssociationLazily
