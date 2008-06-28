abcfc12bd4e7303b84e35eeb9824c32923c563ae
Duncan Beevers <duncanbeevers@gmail.com>
2008-06-27 22:03:25 -0700

    Use proxy_reflection method on association instead of relying on existence of instance variable

--- lib/populate_association_lazily.rb
+++ lib/populate_association_lazily.rb
@@ -15,7 +15,7 @@ module PopulateAssociationLazily
         def replace_with_populate_from_hash *args
           obj = args.shift
           new_obj = obj.kind_of?(Hash) ? new_record(true) { |klass| klass.new(obj) } : obj
-          new_obj = new_obj ? new_obj : @reflection.klass.new(obj)
+          new_obj = new_obj ? new_obj : proxy_reflection.klass.new(obj)
           replace_without_populate_from_hash *args.unshift(new_obj)
         end
         alias_method_chain :replace, :populate_from_hash
